c14d2733c3083d452e9cd2a5e486cfd78c2da500
clinton <clinton.vanry@gmail.com>
2019-10-13 19:39:07 +0100

    email parser

--- email_parser/email_parser/email_parser.cpp
+++ email_parser/email_parser/email_parser.cpp
@@ -1,20 +1,174 @@
 // email_parser.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include <iostream> 
+#include <fstream> 
+#include <string>
+#include <utility>
+#include <vector>
 
-int main()
+using namespace std;
+
+class header_body
 {
-    std::cout << "Hello World!\n";
+	string body;
+public:
+	header_body() = default;
+	header_body(string b) : body(std::move(b)) {}
+	string get_body() const { return body; }
+};
+
+// RFC 5322
+// headers are one per line in form name : value
+//   may be "folded white space"
+//   may contain subitems
+//   not unique and later ones may add to or supercede earlier ones 
+// body is after  first blank line after headers, ignore newlines
+// 
+class email
+{
+	using iter = vector<pair<string, header_body>>::iterator;
+	vector<pair<string, header_body>> headers;
+	string body;
+
+public:
+	email() : body("") {}
+
+	// accessors 
+	string get_body() const { return body; }
+
+	string get_headers() const
+	{
+		// aggregate all headers as a single string
+		string all;
+		for (auto a : headers)
+		{
+			all += a.first + ": " + a.second.get_body();
+			all += "\n";
+		}
+		return all;
+	}
+	
+	iter begin() { return headers.begin(); }
+	iter end() { return headers.end(); }
+
+	// two stage construction 
+	void parse(istream& fin);
+private:
+	void process_headers(const vector<string>& lines);
+};
+
+void email::parse(istream& fin)
+{
+	string line;
+	vector<string> headerLines;
+	while (getline(fin, line))
+	{
+		if (line.empty())
+		{
+			// end of headers 
+			break;
+		}
+		headerLines.push_back(line);
+	}
+
+	process_headers(headerLines);
+
+	while (getline(fin, line))
+	{
+		if (line.empty()) body.append("\n");
+		else body.append(line);
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void email::process_headers(const vector<string>& lines)
+{
+	string header = "";
+	string body = "";
+	for (string line : lines)
+	{
+		if (isspace(line[0])) body.append(line);
+		else
+		{
+			if (!header.empty())
+			{
+				headers.push_back(make_pair(header, body));
+				header.clear();
+				body.clear();
+			}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+			size_t pos = line.find(':');
+			header = line.substr(0, pos);
+			pos++;
+			while (isspace(line[pos])) pos++;
+			body = line.substr(pos);
+		}
+	}
+
+	if (!header.empty())
+	{
+		headers.push_back(make_pair(header, body));
+	}
+}
+
+
+void print_usage()
+{
+	cout << "usage: email_parser file" << "\n";
+	cout << "where file is the path to a file" << "\n";
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc <= 1)
+	{
+		print_usage();
+		return 1;
+	}
+
+	ifstream filestream;
+	filestream.open(argv[1], ios_base::in);
+	if (!filestream.is_open())
+	{
+		print_usage();
+		cout << "cannot open " << argv[1] << "\n";
+		return 1;
+	}
+
+	email eml;
+	eml.parse(filestream);
+
+	cout << eml.get_headers();
+	
+	/*
+	for (auto header : eml)
+	{
+		cout << header.first << " : ";
+		vector<pair<string, string>> subItems = header.second.subitems();
+		if (subItems.size() == 0)
+		{
+			cout << header.second.get_body() << "\n";
+		}
+		else
+		{
+			cout << "\n";
+			for (auto sub : subItems)
+			{
+				cout << "   " << sub.first;
+				if (!sub.second.empty())
+				{
+					cout << " = " << sub.second;
+				}
+				cout << "\n";
+			}
+		}
+	}
+	*/
+	
+	
+	cout << "\n";
+	cout << eml.get_body() << endl;
+
+	return 0;
+}
+
