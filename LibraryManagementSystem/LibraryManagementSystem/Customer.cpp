cdc325178a2788f83b1a798f5b4166f94d594e8a
clinton <clinton.vanry@gmail.com>
2019-11-03 13:33:05 +0000

    fixed casting and used auto

--- LibraryManagementSystem/LibraryManagementSystem/Customer.cpp
+++ LibraryManagementSystem/LibraryManagementSystem/Customer.cpp
@@ -1,8 +1,8 @@
-#include "Customer.h"
 #include <utility>
 #include <string>
 #include <fstream>
 #include "Library.h"
+#include "Customer.h"
 
 using namespace  std;
 
@@ -19,20 +19,21 @@ void Customer::read(std::ifstream& inStream)
 	getline(inStream, m_name);
 	getline(inStream, m_address);
 
-	{ int borrowSetSize;
+	int borrowSetSize;
 	inStream.read(reinterpret_cast<char*>(&borrowSetSize), sizeof borrowSetSize);
 
-	for (int count = 0; count < borrowSetSize; ++count) {
+	for (auto count = 0; count < borrowSetSize; ++count) 
+	{
 		int bookId;
 		inStream.read(reinterpret_cast<char*>(&bookId), sizeof bookId);
 		m_loanSet.insert(bookId);
 	}
-	}
+	
 
 	int reserveListSize;
 	inStream.read(reinterpret_cast<char*>(&reserveListSize),sizeof reserveListSize);
 
-	for (int count = 0; count < reserveListSize; ++count) 
+	for (auto count = 0; count < reserveListSize; ++count) 
 	{
 		int bookId;
 		inStream.read(reinterpret_cast<char*>(&bookId), sizeof bookId);
@@ -43,48 +44,52 @@ void Customer::read(std::ifstream& inStream)
 
 void Customer::write(std::ofstream& outStream) const
 {
-	outStream.write((char*)&m_customerId, sizeof m_customerId);
+	outStream.write(reinterpret_cast<char*>(const_cast<int*>(&m_customerId)), sizeof m_customerId);
 	outStream << m_name << endl;
 	outStream << m_address << endl;
 
-	{ int borrowSetSize = m_loanSet.size();
-	outStream.write((char*)&borrowSetSize, sizeof borrowSetSize);
+	auto borrowSetSize = m_loanSet.size();
+	outStream.write(reinterpret_cast<char*>(const_cast<unsigned*>(&borrowSetSize)), sizeof borrowSetSize);
 
-	for (int bookId : m_loanSet) {
-		outStream.write((char*)&bookId, sizeof bookId);
-	}
-	}
-
-	int reserveListSize = m_reservationSet.size();
-	outStream.write((char*)&reserveListSize,sizeof reserveListSize);
-
-	for (int bookId : m_reservationSet) 
+	for (auto bookId : m_loanSet) 
 	{
-		outStream.write((char*)&bookId, sizeof bookId);
+		outStream.write(reinterpret_cast<char*>(const_cast<int*>(&bookId)), sizeof bookId);
+	}
+	
+
+	auto reserveListSize = m_reservationSet.size();
+	outStream.write(reinterpret_cast<char*>(const_cast<unsigned*>(&reserveListSize)),sizeof reserveListSize);
+
+	for (auto bookId : m_reservationSet) 
+	{
+		outStream.write(reinterpret_cast<char*>(const_cast<int*>(&bookId)), sizeof bookId);
 	}
 	
 }
 
 std::ostream& operator<<(std::ostream& outStream, const Customer& customer)
 {
-	outStream << customer.m_customerId << ". " << customer.m_name
-		<< ", " << customer.m_address << ".";
+	outStream << customer.m_customerId << ". " << customer.m_name << ", " << customer.m_address << ".";
 
-	if (!customer.m_loanSet.empty()) {
+	if (!customer.m_loanSet.empty()) 
+	{
 		outStream << endl << "  Borrowed books: ";
 
-		bool first = true;
-		for (int bookId : customer.m_loanSet) {
+		auto first = true;
+		for (int bookId : customer.m_loanSet) 
+		{
 			outStream << (first ? "" : ",") <<  Library::s_bookMap[bookId].Author();
 			first = false;
 		}
 	}
 
-	if (!customer.m_reservationSet.empty()) {
+	if (!customer.m_reservationSet.empty()) 
+	{
 		outStream << endl << "  Reserved books: ";
 
-		bool first = true;
-		for (int bookId : customer.m_reservationSet) {
+		auto first = true;
+		for (auto bookId : customer.m_reservationSet) 
+		{
 			outStream << (first ? "" : ",") <<  Library::s_bookMap[bookId].Title();
 			first = false;
 		}
